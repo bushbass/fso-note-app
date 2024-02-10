f771529e11b367c130b531c9224476efdd46b121
Alex Nielsen <alex@bigal.org>
2024-02-10 13:13:55 -0500

    https://fullstackopen.com/en/part9/typing_an_express_app#exercises-9-12-9-13

--- src/services/diaryService.ts
+++ src/services/diaryService.ts
@@ -1,11 +1,16 @@
 import diaryData from '../../data/entries';
 
-import { DiaryEntry, NonSensitiveDiaryEntry } from '../types';
+import { DiaryEntry, NonSensitiveDiaryEntry, NewDiaryEntry } from '../types';
 
 const diaries: DiaryEntry[] = diaryData;
 
 const getEntries = (): DiaryEntry[] => {
     return diaries;
+};
+
+const findById = (id: number): DiaryEntry | undefined => {
+    const entry = diaries.find(d => d.id === id);
+    return entry;
 };
 
 const getNonSensitiveEntries = (): NonSensitiveDiaryEntry[] => {
@@ -17,12 +22,26 @@ const getNonSensitiveEntries = (): NonSensitiveDiaryEntry[] => {
     }));
 };
 
-const addDiary = () => {
-    return null;
+
+const addDiary = (
+    entry: NewDiaryEntry
+): DiaryEntry => {
+    const { date, weather, visibility, comment } = entry;
+    const newDiaryEntry = {
+        id: Math.max(...diaries.map(d => d.id)) + 1,
+        date,
+        weather,
+        visibility,
+        comment,
+    };
+
+    diaries.push(newDiaryEntry);
+
+    return newDiaryEntry;
 };
 
 export default {
-    getEntries,
+    getEntries, findById,
     getNonSensitiveEntries,
     addDiary
 };
